f7eec35b6cce0c5504848449654aca8048b63454
Levrone2308 <harshpatel23082004@gmail.com>
2025-07-11 17:18:37 +0530

    new commit4

--- api/app.py
+++ api/app.py
@@ -2,14 +2,16 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# Connect to MongoDB
 client = MongoClient("mongodb://mongo:27017/")
 db = client["news"]
 collection = db["headlines"]
 
-@app.route("/news", methods=["GET"])
-def get_news():
-    data = list(collection.find({}, {"_id": 0}))
-    return jsonify(data)
+@app.route("/headlines", methods=["GET"])
+def get_headlines():
+    headlines = list(collection.find({}, {"_id": 0}))
+    return jsonify(headlines)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
